6658be766c07315f07c9edc7c0d54c64f5f46318
Kacper <wasiak.kacper11@gmail.com>
2023-11-22 21:36:22 +0100

    Dodawanie elementów listy

--- js/board.js
+++ js/board.js
@@ -1,14 +1,41 @@
-let itemName = document.querySelector("#name");
-let addBtn = document.querySelector("#add");
-let tab = [];
+//Dodanie nowego elementu do listy
 
-addBtn.addEventListener('click', ()=>{
-    if(itemName.nodeValue != ''){
-        let newItem = document.createElement('div');
-        newItem.className = 'list-item';
+let container = document.getElementById("todo-wrapper");
+let itemName = document.getElementById("item-name");
+let confirmBtn = document.getElementById("add-btn");
+let todoTab = [];
 
-        for(let i = 0; i < itemName.length; i++) {
-            newItem.innerHTML = newItem.value;
+    confirmBtn.addEventListener('click', ()=>{
+
+    if(itemName.value != ''){
+
+        todoTab.push(itemName.value);
+
+        var newItem = document.createElement('div');
+        newItem.setAttribute('draggable', 'true');
+        newItem.className = "list-item";
+
+        for(let i = 0; i < todoTab.length; i++){
+
+            newItem.innerHTML = `<img src="dodatkowe pliki/avatar.png" alt="zdjęcie profilowe"/><p>${itemName.value}</p><button class="material-symbols-outlined" id="delete" onclick="removeItem()">close</button>`;
+
+            container.appendChild(newItem);
+
         }
+
     }
-})
+
+    //usunięcie elementu listy
+    if(todoTab.length > 0){
+        let item = document.querySelectorAll(".list-item");
+
+            for(let j = 0; j < item.length; j++){
+
+
+                function removeItem(){
+                    container.removeChild(item[j]);
+                };
+            }
+    }
+
+});
