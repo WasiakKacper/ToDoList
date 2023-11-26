515d18621a7a0299779e634940f635c39c0a8c3d
Kacper <wasiak.kacper11@gmail.com>
2023-11-26 12:53:43 +0100

    Dodawanie i usuwanie elementów listy

--- js/board.js
+++ js/board.js
@@ -1,41 +1,49 @@
-//Dodanie nowego elementu do listy
-
+//DDODAWANIE ELEMENTU DO LISTY ORAZ JEGO USUWANIE
+//Tworzenie zmiennych kontenera, nazwy elemenu oraz przycisku
 let container = document.getElementById("todo-wrapper");
-let itemName = document.getElementById("item-name");
-let confirmBtn = document.getElementById("add-btn");
-let todoTab = [];
+let list_el_value = document.getElementById("item-name");
+let add_btn = document.getElementById("add-btn");
 
-    confirmBtn.addEventListener('click', ()=>{
+add_btn.addEventListener('click', function addItem(){//Funkcja tworząca element
 
-    if(itemName.value != ''){
+    if(list_el_value.value != ''){ //Jesli pole nie jest puste utwórz element
+        
+        //Tworzenie elementu
+        let new_item = document.createElement('div');
+        new_item.classList.add("list-item");
+        new_item.setAttribute('draggable', 'true');
+        new_item.innerHTML = `<img src="dodatkowe pliki/avatar.png" alt="Zdjęcie profilowe"/><p>${list_el_value.value}</p>`;
+        container.appendChild(new_item);
 
-        todoTab.push(itemName.value);
+        //Tworzenie przycisku uswania
+        let delete_btn = document.createElement('button');
+        delete_btn.classList.add('material-symbols-outlined')
+        delete_btn.innerHTML = "close";
+        new_item.appendChild(delete_btn);
 
-        var newItem = document.createElement('div');
-        newItem.setAttribute('draggable', 'true');
-        newItem.className = "list-item";
+        delete_btn.addEventListener('click', ()=>{ //Funkcja usuwająca element
+            
+            container.removeChild(new_item);//Usunięcie elementu
 
-        for(let i = 0; i < todoTab.length; i++){
-
-            newItem.innerHTML = `<img src="dodatkowe pliki/avatar.png" alt="zdjęcie profilowe"/><p>${itemName.value}</p><button class="material-symbols-outlined" id="delete" onclick="removeItem()">close</button>`;
-
-            container.appendChild(newItem);
-
-        }
+        });
 
     }
 
-    //usunięcie elementu listy
-    if(todoTab.length > 0){
-        let item = document.querySelectorAll(".list-item");
+    else{ //Jesli pole jesy puste wyświetl kominukat
+        let modal = document.getElementById('alert-modal');
+        modal.innerHTML = '<p>Musisz nazwac elemnt żeby go utworzyć !</p><button id="confirm">Ok</button>';
+        modal.classList.add('active');
+        let close_btn = document.getElementById('confirm');
 
-            for(let j = 0; j < item.length; j++){
+        close_btn.addEventListener('click', ()=>{
 
+            modal.classList.remove('active');
 
-                function removeItem(){
-                    container.removeChild(item[j]);
-                };
-            }
+        });
+
     }
-
 });
+
+//PRZENOSZENIE ELEMENTU MIĘDZY LISTAMI
+
+
