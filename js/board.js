688cf1b1c45edeb2e0984b753a5279b9dddec5ae
Kacper <wasiak.kacper11@gmail.com>
2023-12-02 21:59:55 +0100

    to do list ukończone

--- js/board.js
+++ js/board.js
@@ -1,101 +1,63 @@
-/*DDODAWANIE ELEMENTU DO LISTY ORAZ JEGO USUWANIE*/
-//Tworzenie zmiennych kontenera, nazwy elemenu oraz przycisku
-let container = document.getElementById("todo_wrapper");
-let list_el_value = document.getElementById("item-name");
-let add_btn = document.getElementById("add-btn");
 
-add_btn.addEventListener('click', function addItem(){//Funkcja tworząca element
+        let list_items = document.querySelectorAll('.list-item');
 
-    if(list_el_value.value != ''){ //Jesli pole nie jest puste utwórz element
-        
-        //Tworzenie elementu
-        let new_item = document.createElement('div');
-        new_item.classList.add("list-item");
-        new_item.setAttribute('draggable', 'true');
-        new_item.innerHTML = `<p>${list_el_value.value}</p>`;
-        container.appendChild(new_item);
-        list_el_value.value = '';
-
-        //Tworzenie przycisku uswania
-        let delete_btn = document.createElement('button');
-        delete_btn.classList.add('material-symbols-outlined')
-        delete_btn.innerHTML = "close";
-        new_item.appendChild(delete_btn);
-
-        delete_btn.addEventListener('click', ()=>{ //Funkcja usuwająca element
+        list_items.forEach((task) => {
             
-            container.removeChild(new_item);//Usunięcie elementu
-
-        });
-
-        /* PRZENOSZENIE ELEMENTÓW MIĘDZY LISTAMI */
-        let list = document.getElementsByClassName('list-item');
-        let todo_box = document.getElementById('to-do');
-        let in_progress_box = document.getElementById('in-progress');
-        let done_box = document.getElementById('done')
-
-        for(item of list){
-
-            item.addEventListener('dragstart', function(e){
-
-                let dragging_elem = e.target;
-
-                /* BOX NR 1 */
-                todo_box.addEventListener('dragover', function(e){
-
-                    e.preventDefault();
-
-                });
-
-                todo_box.addEventListener('drop', function(e){
-                    let todo_wrapper = document.getElementById('todo_wrapper');
-                    todo_wrapper.appendChild(dragging_elem);
-                    dragging_elem = null;
-
-                });
-
-                /* BOX NR 2 */
-                in_progress_box.addEventListener('dragover', function(e){
-
-                    e.preventDefault();
-
-                });
-
-                in_progress_box.addEventListener('drop', function(e){
-                    let in_progress_wrapper = document.getElementById('in_progres_wrapper');
-                    in_progress_wrapper.appendChild(dragging_elem);
-                    dragging_elem = null;
-
-                });
-
-                /* BOX NR 3 */
-                done_box.addEventListener('dragover', function(e){
-                    e.preventDefault();
-                });
-
-                done_box.addEventListener('drop', function(e){
-                    let done_wrapper = document.getElementById('done_wrapper');
-                    done_wrapper.appendChild(dragging_elem);
-                    dragging_elem = null;
-
-                });
-
+            task.addEventListener('dragstart', ()=>{
+                task.classList.add('dragging');
+    
+            });
+    
+            task.addEventListener('dragend', ()=>{
+                task.classList.remove('dragging');
+    
             });
 
-        }
 
-    }
-
-    else{ //Jesli pole jesy puste wyświetl kominukat
-        let modal = document.getElementById('alert-modal');
-        modal.classList.add('active');
-        let close_btn = document.getElementById('confirm');
-
-        close_btn.addEventListener('click', ()=>{
-
-            modal.classList.remove('active');
 
         });
 
-    }
-});
+        
+        let items_wrappers = document.querySelectorAll('.list-items-wrapper');
+
+        items_wrappers.forEach((zone) =>{
+            
+            zone.addEventListener('dragover', (e)=> {
+                e.preventDefault();
+
+                const bottomTask = insertAboveTask(zone, e.clientY);
+                const curTask = document.querySelector('.dragging');
+
+                if(!bottomTask){
+                    zone.appendChild(curTask);
+                }
+                else{
+                    zone.insertBefore(curTask, bottomTask)
+                }
+                
+            });
+
+        });
+
+        const insertAboveTask = (zone, mouseY) => {
+
+            const els = zone.querySelectorAll('.task:not(.dragging)');
+
+            let closestTask = null;
+            let closestOffSet = Number.NEGATIVE_INFINITY;
+            
+            els.forEach((task)=>{
+
+                const { top } = task.getBoudnigClientRect();
+                const offset = mouseY - top;
+
+                if(offset < 0 && offset > closestOffSet){
+
+                    closestOffSet = offset;
+                    closestTask = task;
+
+                }
+
+            });
+            return closestTask;
+        };
