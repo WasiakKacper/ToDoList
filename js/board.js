f250bb431828488506bd0a1cd4a5d9b5aad684b5
Kacper <wasiak.kacper11@gmail.com>
2023-12-10 09:17:35 +0100

    Dodanie zadania na wciśnięcie Enter

--- js/board.js
+++ js/board.js
@@ -1,22 +1,28 @@
 let list_items = document.querySelectorAll('.list-item');
 
-/* Na komputerze */
+
 list_items.forEach((task) => {
-    
-    task.addEventListener('dragstart', ()=>{
+
+    task.addEventListener('dragstart', dragStart);
+    task.addEventListener('touchstart', dragStart);
+
+    function dragStart(){
+
         task.classList.add('dragging');
 
-    });
+        task.addEventListener('dragend', ()=>{
+            task.classList.remove('dragging');
 
-    task.addEventListener('dragend', ()=>{
-        task.classList.remove('dragging');
+        });
 
-    });
+        task.addEventListener('touchend', ()=>{
+            task.classList.remove('dragging');
 
-
+        });
+    
+    }
 
 });
-
 
 let items_wrappers = document.querySelectorAll('.list-items-wrapper');
 
@@ -48,10 +54,10 @@ const insertAboveTask = (zone, mouseY) => {
     
     els.forEach((task)=>{
 
-        const { top } = task.getBoudnigClientRect();
+        const { top } = task.getBoundingClientRect();
         const offset = mouseY - top;
 
-        if(offset < 0 && offset > closestOffSet){
+        if(offset > 0 && offset > closestOffSet){
 
             closestOffSet = offset;
             closestTask = task;
@@ -63,6 +69,8 @@ const insertAboveTask = (zone, mouseY) => {
 };
 
 
+
+/* Dodanie na kliknięcie */
 let item_value = document.getElementById('item-name');
 let add_btn = document.getElementById('add-btn');
 
@@ -121,6 +129,81 @@ add_btn.addEventListener('click', (e)=>{
             modal.classList.remove('active');
 
         });
+
+    }
+
+});
+
+
+
+/* Dodanie na wciśnięcie przycisku enter */
+document.addEventListener('keypress', (e)=>{
+    
+    if(e.key === 'Enter'){
+
+        e.preventDefault();
+
+        const value = item_value.value;
+        const container = document.getElementById('todo_wrapper');
+    
+        if(value != ''){
+    
+            const task = document.createElement('div');
+            task.classList.add('list-item');
+            task.setAttribute('draggable', 'true');
+            task.innerHTML = value;
+    
+            task.addEventListener('dragstart', ()=>{
+    
+                task.classList.add('dragging');
+    
+            });
+    
+            task.addEventListener('dragend', ()=>{
+                
+                task.classList.remove('dragging');
+    
+            });
+    
+            container.appendChild(task);
+    
+            const del_btn = document.createElement('button');
+            del_btn.classList.add('material-symbols-outlined');
+            del_btn.innerHTML = 'close';
+            task.appendChild(del_btn);
+    
+            del_btn.addEventListener('click', (e)=>{
+    
+                e.preventDefault();
+                del_btn.parentElement.remove();
+    
+            });
+    
+            item_value.value = "";
+    
+        }
+    
+        else{
+    
+            let modal = document.getElementById('alert-modal');
+            modal.classList.add('active');
+            let close_btn = document.getElementById('confirm');
+    
+            close_btn.addEventListener('click', ()=>{
+    
+                modal.classList.remove('active');
+    
+            });
+
+            document.addEventListener('keypress', (e)=>{
+    
+                if(e.key === 'Enter'){
+                modal.classList.remove('active');
+                
+                }    
+            });
+    
+        }
 
     }
 
