0aa021620390c5b836eb8070401f413b009e71e7
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-07-26 10:45:20 +0200

    todolist local storage 1.1

--- js/localStorage.js
+++ js/localStorage.js
@@ -10,28 +10,28 @@ document.addEventListener('DOMContentLoaded', ()=>{
         tasks = Object.keys(dataFromStorage).map((key)=>{
             return dataFromStorage[key];
         });
+
+        tasks.forEach(tasks => {
+            if(tasks != " "){
+                const task = document.createElement('div');
+                task.classList.add('list-item');
+                task.setAttribute('draggable', 'true');
+                task.innerHTML = tasks;
+    
+                const del_btn = document.createElement('button');
+                del_btn.classList.add('material-symbols-outlined');
+                del_btn.innerHTML = 'close';
+                task.appendChild(del_btn);
+
+                del_btn.addEventListener('click', (e)=>{
+                    e.preventDefault();
+                    del_btn.parentElement.remove();
+                });
+            
+                container.appendChild(task);
+            }
+        });
     }
-
-    tasks.forEach(tasks => {
-        if(tasks != " "){
-            const task = document.createElement('div');
-            task.classList.add('list-item');
-            task.setAttribute('draggable', 'true');
-            task.innerHTML = tasks;
-
-            const del_btn = document.createElement('button');
-            del_btn.classList.add('material-symbols-outlined');
-            del_btn.innerHTML = 'close';
-            task.appendChild(del_btn);
-
-            del_btn.addEventListener('click', (e)=>{
-
-                e.preventDefault();
-                del_btn.parentElement.remove();
-                
-            });
-        
-            container.appendChild(task);
-        }
-    });
 });
+const task = document.querySelectorAll('.list-item');
+const delBtn = task.lastElementChild;
