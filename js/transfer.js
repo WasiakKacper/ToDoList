ae3990d5e875c5118a427c012d6f864d2cf38fa0
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-08-09 13:53:59 +0200

    transfering between boxes is working now

--- js/transfer.js
+++ js/transfer.js
@@ -2,8 +2,33 @@ const done = document.getElementById('done_wrapper');
 
 done.addEventListener('dragover', (e)=>{
     e.preventDefault();
-    const currentTask = document.querySelectorAll('.dragging');
+
     done.addEventListener('drop', ()=>{
+        const currentTask = document.querySelector('.dragging');
         done.appendChild(currentTask);
+        currentTask.classList.add('done');
+
+        let tasks = [];
+
+/*         function cutTask(){
+            tasks = JSON.parse(localStorage.getItem('task'));
+            const task = document.querySelector('.done');
+            task.
+        }
+        cutTask(); */
+    });
+});
+
+
+
+const todo = document.getElementById('todo_wrapper');
+
+todo.addEventListener('dragover', (e)=>{
+    e.preventDefault();
+
+    todo.addEventListener('drop', ()=>{
+        const currentTask = document.querySelector('.dragging');
+        todo.appendChild(currentTask);
+        currentTask.classList.remove('done');
     });
 });
