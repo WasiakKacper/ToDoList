5d274ccde6acb403939a58cbb39b95513cbfec7e
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-08-10 11:17:56 +0200

    Working on addind and deleting tasks from localStorage when element is trasfering between boxes

--- js/transfer.js
+++ js/transfer.js
@@ -2,21 +2,28 @@ const done = document.getElementById('done_wrapper');
 
 done.addEventListener('dragover', (e)=>{
     e.preventDefault();
+});
 
-    done.addEventListener('drop', ()=>{
-        const currentTask = document.querySelector('.dragging');
-        done.appendChild(currentTask);
-        currentTask.classList.add('done');
+done.addEventListener('drop', (e)=>{
+    e.preventDefault();
 
-        let tasks = [];
+    const currentTask = document.querySelector('.dragging');
+    done.appendChild(currentTask);
+    currentTask.classList.add('done');
 
-/*         function cutTask(){
-            tasks = JSON.parse(localStorage.getItem('task'));
-            const task = document.querySelector('.done');
-            task.
-        }
-        cutTask(); */
-    });
+    //Cut form sotrage
+    let tasks = [];
+
+    function cutFromStorage(){
+        tasks = JSON.parse(localStorage.getItem('task'));
+
+        let value = document.querySelector('.done');
+        
+        tasks.shift(value);
+        localStorage.setItem('task', JSON.stringify(tasks));
+    }
+    cutFromStorage();
+
 });
 
 
@@ -25,10 +32,22 @@ const todo = document.getElementById('todo_wrapper');
 
 todo.addEventListener('dragover', (e)=>{
     e.preventDefault();
+});
 
-    todo.addEventListener('drop', ()=>{
-        const currentTask = document.querySelector('.dragging');
-        todo.appendChild(currentTask);
-        currentTask.classList.remove('done');
-    });
+todo.addEventListener('drop', ()=>{
+    const currentTask = document.querySelector('.dragging');
+    todo.appendChild(currentTask);
+    currentTask.classList.remove('done');
+
+//Function saveing tasks in local storage
+    let tasks = [];
+
+    function saveInStorage(){
+        if(localStorage.getItem('task') !== null){
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+
+            
+        }
+    }
+    saveInStorage();
 });
