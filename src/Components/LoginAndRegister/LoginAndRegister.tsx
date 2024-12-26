4a01e94b303df23cbda854bbe46f420b99020119
Kacper Wasiak <wasiak.kacper11@gmail.com>
2024-12-26 11:47:48 +0100

    Login and register site 1.1

--- src/Components/LoginAndRegister/LoginAndRegister.tsx
+++ src/Components/LoginAndRegister/LoginAndRegister.tsx
@@ -2,18 +2,54 @@ import { useState } from "react";
 import "./LoginAndRegister.css";
 import Logo from "/Images/Logo.svg";
 
+//Login and Register component
 const LoginAndRegister = () => {
   const [isActive, setIsActive] = useState(false);
   const [visible, setVisibility] = useState(false);
 
+  //Sleep function
   const sleep = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
+  //Site render function
   const handleRender = async () => {
     setIsActive(!isActive);
     await sleep(100);
     setVisibility(!visible);
+  };
+
+  //Getting value of input
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isSamePassword, setIsSamePassword] = useState("");
+
+  const handleGetLoginValues = () => {
+    if (email != "" && password != "") {
+      alert(email + " " + password);
+      setEmail("");
+      setPassword("");
+    } else {
+      alert("Pole email lub hasło jest puste");
+      setEmail("");
+      setPassword("");
+    }
+  };
+
+  const handleGetRegisterValues = () => {
+    if (email != "" && password != "" && isSamePassword != "") {
+      if (isSamePassword == password) {
+        alert(email + " " + password);
+        setEmail("");
+        setPassword("");
+        setIsSamePassword("");
+      } else {
+        alert("Hasłą różnią się!");
+      }
+    } else {
+      alert("Któreś z pól jest puste");
+    }
   };
 
   return (
@@ -27,15 +63,27 @@ const LoginAndRegister = () => {
             <h1>Logowanie</h1>
             <div className="inputLabelContainer">
               <label htmlFor="email">Adres e-mail:</label>
-              <input type="email" name="email" className="inputs" />
+              <input
+                type="email"
+                name="email"
+                className="inputs"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
 
             <div className="inputLabelContainer">
               <label htmlFor="password">Hasło:</label>
-              <input type="password" className="inputs" name="password" />
+              <input
+                type="password"
+                className="inputs"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
 
-            <button onSubmit={(e) => e.preventDefault()}>Zaloguj się!</button>
+            <button onClick={handleGetLoginValues}>Zaloguj się!</button>
           </div>
         ) : (
           <div id="login" className="form">
@@ -49,20 +97,38 @@ const LoginAndRegister = () => {
 
             <div className="inputLabelContainer">
               <label htmlFor="email">Adres e-mail:</label>
-              <input type="email" name="email" className="inputs" />
+              <input
+                type="email"
+                name="email"
+                className="inputs"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
 
             <div className="inputLabelContainer">
               <label htmlFor="password">Hasło:</label>
-              <input type="password" className="inputs" name="password" />
+              <input
+                type="password"
+                className="inputs"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
 
             <div className="inputLabelContainer">
               <label htmlFor="password">Powtórz hasło:</label>
-              <input type="password" className="inputs" name="password" />
+              <input
+                type="password"
+                className="inputs"
+                name="password"
+                value={isSamePassword}
+                onChange={(e) => setIsSamePassword(e.target.value)}
+              />
             </div>
 
-            <button>Zarejstruj się!</button>
+            <button onClick={handleGetRegisterValues}>Zarejstruj się!</button>
           </div>
         ) : (
           <div id="register" className="form">
